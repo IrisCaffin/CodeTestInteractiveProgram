1af5ffc184fe1c27eef04a04acd06881fa849a6f
Iris Caffin <iriscaffin@gmail.com>
2016-05-30 19:40:45 -0700

    Program works but can only enter one integer it won't continue

---
+++ InteractiveProgram/InteractiveProgram/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteractiveProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer: ");
+
+            string line = Console.ReadLine();
+
+            int value;
+            Int32.TryParse(line, out value);
+
+            if (value < 10)
+            {
+                Console.WriteLine("This number is too small.");
+            }
+            else
+            {
+                Console.WriteLine("This number is big enough");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
